7a9b7d39184e036c7f975402a497d4f9a0cbf7be
Sophia <sz7155@princeton.edu>
2025-02-19 11:59:07 -0500

    very beginning of the TD_setup flow

--- driver/flows/flows.c
+++ driver/flows/flows.c
@@ -7,4 +7,9 @@ api_error_type TD_setup() {
 
     __CPROVER_havoc_object(&target_tdr_pa);
     __CPROVER_havoc_object(&hkid_info);
+    error = tdh_mng_create(target_tdr_pa, hkid_info); 
+    __CPROVER_assert(error == TDX_SUCCESS );
+    error = tdh_mng_key_config(target_tdr_pa);
+    __CPROVER_assert(error == TDX_SUCCESS );
+    
 }
