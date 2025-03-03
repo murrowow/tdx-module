910535e5a844c5bdd1329a6916791bb91bc7a64d
Sophia <sz7155@princeton.edu>
2025-03-02 22:26:30 -0500

    removing old files

--- helpers/cbmc_helper.h
+++
@@ -1,8 +0,0 @@
-#ifndef CBMCHEADER_H
-#define CBMCHEADER_H
-
-#include "src/common/helpers/helpers.h"
-
-pamt_entry_t * get_pamt_entry(uint64_t target_tdr_pa, hkid_api_input_t hkid_info);
-
-#endif 
